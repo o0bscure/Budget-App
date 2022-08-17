90c9b163a450d1ee3c5418e56ce35e386144d2a0
M.Ghanem <86367386+o0bscure@users.noreply.github.com>
2022-08-17 18:43:15 +0300

    Update budget.py
    1 - fixed a bug in the transfer method where it would deposit the amount to the destination category even if the the withdrawal to the target category failed.
    
    2 - added a description to the deposit part of the transfer method

--- budget.py
+++ budget.py
@@ -35,7 +35,7 @@ class Category:
         
     def withdraw(self,amount,description=""):
         #keep track of the balance
-        if amount < self.balance:    #fixed bug here where the value of the balance can be in negative.
+        if amount <= self.balance:    #fixed bug here where the value of the balance can be in negative.
             #THIS NEEDS FIXING!!!
             self.balance = self.balance - amount
             #making sure i add nothing to the ledger if there isnt enough balance(not enough deposited money to withdraw from)
@@ -59,30 +59,30 @@ class Category:
         #A get_balance method that returns the current balance of the budget category 
         #based on the deposits and withdrawals that have occurred
         
-    def transfer(self,transto,amount:int,description=""):
-        if not isinstance(transto,Category):
-            print(f"{transto} isn't a category")
+    def transfer(self,destination,amount:int,description=""):
+        #first check if the given argument to transfer the money to (destination) is an actual category object
+        if not isinstance(destination,Category):
+            print(f"{destination} isn't a category")
             return False
-        #that transto has to be an object(category)
-        #first withdraw the amount to be transefered from the desired category.
-        self.withdraw(amount)
-        #check within the categories list if the category you're transfering to exist
-        #assert transto in Category.categories, f"{transto} isn't a category!!"
-        for object in Category.categories:
-            if transto == object:
-                #if the targetted category(object) exist, deposite the money you trasfered
-                try:
-                    object.deposit(amount)
-                    return True
-                except:
-                    return False
-            
-
-        #A transfer method that accepts an amount and another budget category as arguments. 
+        else: 
+            #first withdraw the amount to be transefered from the desired category.
+            #self.withdraw(amount)
+            #if the withdraw operation has failed, return false
+            #it seems like once the function is put in an conditional statement, it actually runs!(in the that if statement the withdrawal function already being executed)
+            if self.withdraw(amount) == False:
+                print(f"not enough balance in {self.name} category")
+                return False
+            else:
+                #check within the categories list if the category you're transfering to exists
+                for object in Category.categories:
+                    if destination == object:
+                        #if the targetted category(object) exist, deposite the money you trasfered
+                        object.deposit(amount)
+                        print(f"Transfer {amount} from [{self.name}]")
+                        return True       
         #The method should add a withdrawal with the amount and the description "Transfer to [Destination Budget Category]".
         #The method should then add a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]".
         #If there are not enough funds, nothing should be added to either ledgers.
-        # This method should return True if the transfer took place, and False otherwise.
     
     def check_funds():
         #A check_funds method that accepts an amount as an argument.
@@ -106,13 +106,11 @@ entertainment.deposit(300,"Money for hangouts")
 
 
 
-print(food.get_balance())
-food.withdraw(500)
-#print(clothing.get_balance())
-#food.transfer(clothing,500)
-print(food.get_balance())
-#print(clothing.get_balance())
-
+print("the food balance is :",food.get_balance())
+print("the clothing balance is :",clothing.get_balance())
+food.transfer(clothing,450)
+print("the food balance is :",food.get_balance())
+print("the clothing balance is :",clothing.get_balance())
 
 
 
