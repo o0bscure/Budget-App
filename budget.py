c92f76687e70fb434b32d75552ee6d48c4faf925
M.Ghanem <86367386+o0bscure@users.noreply.github.com>
2022-10-11 18:23:42 +0300

    Update budget.py

--- budget.py
+++ budget.py
@@ -1,6 +1,5 @@
 
 class Category:
-    #a class attribute that can be used on all this class objects
     balance = float()
     categories = []
     #keeps track of all deposted money
@@ -12,7 +11,7 @@ class Category:
         self.name = name
         #create a ledger list for each indivisual Category (object)
         self.ledger = list()
-        Category.categories.append(self) #or self .name ?! , i'll have to look at the latest function requirement later
+        Category.categories.append(self)
 
     def deposit(self,amount,description=""):
         #keep track of all deposited money
@@ -24,7 +23,6 @@ class Category:
         data["amount"] = float(amount)
         data["description"] = description
         #adding the dictionary to the ledger list
-        #hmmm maybe find a way to write that on a csv file (fun)
         self.ledger.append(data)
         
     def withdraw(self,amount,description=""):
@@ -32,28 +30,22 @@ class Category:
         if self.check_funds(amount) == True:
             #withdraw from the category balance
             self.balance = self.balance - amount
-            #making sure i add nothing to the ledger if there isnt enough balance(not enough deposited money to withdraw from)
-            #okay maybe make a condition to add the key and value if the disctionary doesnt exist to avoid overwriting it
-            #you'll have tp manipulate existing ledger in case you wanna withdraw twice
-            #each withdraw will add the object withdrawn from as a dictionary
+            #making sure to add nothing to the ledger if there isnt enough balance
             data = dict()
-            #the amount should be stored in the withdraw ledger as a negative number, did that by adding the minus sign as a string then convert it to string
+            #the amount should be stored in the withdraw ledger as a negative number add the minus sign as a string then convert it to string
             data["amount"] = "-"+ str(amount)
             data["amount"] = float(data["amount"])
             data["description"] = description
             self.ledger.append(data)
-            
-            #add each amount spent in a separate dictionary, to calculate the percentage spent for each category
+            #add each amount spent in a separate dictionary, to calculate the percentage spent for each category(to be used later)
             if f"{self.name}" in Category.total_spent:
                 Category.total_spent[f"{self.name}"] = Category.total_spent[f"{self.name}"] + float(data["amount"])
             else:
-                Category.total_spent[f"{self.name}"] = float(data["amount"])
-                
+                Category.total_spent[f"{self.name}"] = float(data["amount"])                
             return True
         else:
             print("not enough balance")
             return False
-
 
     def get_balance(self):
         return self.balance
@@ -65,7 +57,7 @@ class Category:
             print(f"{destination} isn't a category")
             return False
         else: 
-            #check if the amount to be transfered is available in the target caegory
+            #check if the amount to be transfered is available in the target category
             if self.check_funds(amount) == False:
                 print(f"not enough balance in {self.name} category")
                 return False
@@ -75,36 +67,37 @@ class Category:
                 #check within the categories list if the category you're transfering to exists
                 for object in Category.categories:
                     if destination == object:
-                        #if the targetted category(object) exist, deposite the money you trasfered
+                        #if the targetted category(object) exists, deposite the money you trasfered
                         description = f"Transfer from {self.name}"
                         object.deposit(amount,description)
                         return True       
     
     #a function that tells you if the input amount is availble for that particular category
     def check_funds(self,amount):
-        #check if the amount given is greater than the category balance or not
+        #check if the amount given is greater than the category balance
         if amount > self.balance:
             return False
         else:
             return True
         
     def __str__(self):
+        #create the header
         header = str("*"*30)
         title = self.name
         header = header[int(len(title)):]
         if len(header) < 30 and len(title) == len(self.name):
             header = header[:int(len(header)/2)] + str(self.name) + header[int(len(header)/2):]
-            #header is ready, now do the rest
-        chart = list()
-        #need to make a loop inside that return statement, a loop which goes through the ledger list by index, then print the description and amount keys from each disctionary
-        for index in range(len(self.ledger)):
-            #next line is where each withdraw or deposite will be presented
-            #adjust the position of the amount to be aligned on the right, opposite of the description, without exceeding the lenght of the entire header
-            line = str(self.ledger[index]['description']) + str(f"{self.ledger[index]['amount']:.2f}").rjust(len(header)-len(self.ledger[index]['description']))
-            #once extraced, add it to a list so u can pick from that list later in order to print the result
-            chart.append(line)
             
-        #go through the chart list (by index) which has each line of discripton and amount as an a string item.
+        #prepare the chart
+        chart = list()
+        #loop through the ledger list by index
+        for index in range(len(self.ledger)):
+            #line string is where each withdraw or deposite will be presented,adjust the position of the amount to be aligned to the right, opposite of the description, without exceeding the lenght of the entire header
+            line = str(self.ledger[index]['description']) + str(f"{self.ledger[index]['amount']:.2f}").rjust(len(header)-len(self.ledger[index]['description']))
+            #once extraced, add it to a list
+            chart.append(line)    
+            
+        #loop through the chart list(by index) which has each line of discripton and amount as an a string item.
         result = ""
         for index in range(len(chart)):
             #each time the loop runs, it adds a line from the list to the initial result until there are no more items.
@@ -119,11 +112,10 @@ class Category:
                     #adjust the number value to the right without exceeding the header's length (dont forget to subtract the 2 brackets from the rjust(-2))
                     num = chart[index].split(")")[1].rjust(len(header)-len(second)-2)
                     chart[index] = f"{first}\n({second}){num}"
-                #i could add more conditional statements here, if i want to maniuplute other strings than the brackets
+                #more conditional statements could be added here if needed
                 else:
                     sign = chart[index].find("-")
                     description = chart[index][:sign-1]
-                    print(description)
                     num = chart[index][sign:]
                     description = description[0:len(header)-len(num)]
                     chart[index] = f"{description[:-1]} {num}"
@@ -132,11 +124,9 @@ class Category:
                 pass
             result = f"{result}{chart[index]}\n"
                 
-        #finally add the header to the final output
+        #add the header to the final output
         output = f"{header}\n{result}Total: {self.balance}"
         return output
-
-
 
 #a dictionary that saves each category as key and the percentage base as value
 percentage_dict = dict()
@@ -145,13 +135,14 @@ category_names_length = list()
 def create_spend_chart(z):
     total_withdrawal = 0
     for key,value in Category.total_spent.items():
+        #add the total withdrawn, remove the minus sign from the string
         total_withdrawal = total_withdrawal + abs(value)
-    #calculate the percentage of the spent amount(did that for one category only)
+    #calculate the percentage of the spent amount
     for cat in z:
         #only go through the categories the are spent from (categories stored in total spend dictionary)
         if f"{cat.name}" in Category.total_spent:
+            #take the percentage spent for each category relative to the -total withdrawals-
             quotient = abs(Category.total_spent[f"{cat.name}"])/ total_withdrawal
-            #turns out i had to take the percentage spent for each category relative to the the total withdrawals
             percent = quotient * 100
             percentage_dict[f"{cat.name}"] = int(percent)
             #add the lenght of each category name to a list
@@ -164,19 +155,18 @@ def create_spend_chart(z):
         for cat,num in percentage_dict.items():
             if num > x:
                 per_chart[f"{cat}"] = x + 10
-    #per_chart is the dictionary u should rely on now (it has the normalize version of the percentage chart with values like 0,10,20 .... 100)
+    #per_chart is the dictionary that rounds up the percentage numbers
 
-    #markers to add to the chart
+    #markers to add to the chart, based on the percentage spent
     marker = f"  o"
-    #header
+    #create header
     header = "Percentage spent per category"
-    #the seperating line
+    #create seperating line
     per_line = f"    {'-'*(len(Category.categories)+9)}"
+    #create the upper part of the chart
     upper = ""
     upper_list= []
-
-    #work on the upper part of the chart
-    #for each item in the spent categories (food and clothing in this case)
+    #for each item in the spent categories
     for item in per_chart:
         #go through the list of numbers[0,100]
         pos = 0
@@ -198,36 +188,33 @@ def create_spend_chart(z):
                 if per_chart[item] >= x:
                     upper_list[pos] = upper_list[pos] + marker
             pos = pos + 1
-        #work on the upper part of the chart is done, now for the lower part
-        #loop through the letters is each category
-    
+
     for line in upper_list:
         upper = upper + line + "\n"
 
     lower = ""
+    #loop through the letters is each category
     for i in range(max(category_names_length)):
         try:
             lower = lower+ 6*" "
             for cat in z:
                 if i >= len(cat.name):
-                    #once the position exceedes the length of given category(all category letters have been printed)
+                    #once the position exceedes the length of given category (all category letters have been printed)
                     lower = lower + "   "
                     continue
                 lower = lower + cat.name[i] + "  "
             lower = lower + "\n"
         except Exception as exception:
             print(exception)
-            #need to find a way to go through the index issue, 
- 
     result = f"{header}\n{upper}{per_line}\n{lower}"
     print(result)
 
+#create some objects
 food = Category("Food")
 entertainment=Category("Entertainment")
 business=Category("Business")
 
-
-
+#test several functions (deposit, withdraw and transfer)
 food.deposit(900, "deposit")
 entertainment.deposit(900, "deposit")
 business.deposit(900, "deposit")
@@ -236,7 +223,11 @@ food.transfer(120,business)
 entertainment.withdraw(33.40)
 business.withdraw(10.99)
 
+#print some objects
 print(business)
 print(food)
-#create_spend_chart(Category.categories)
-#the loop keeps going several times after the intended iterations, even if its not printing
+
+#test the create spent chart
+create_spend_chart(Category.categories)
+
+#the loop keeps going several times after the intended iterations, even if its not printing (needs to be fixed later!)
